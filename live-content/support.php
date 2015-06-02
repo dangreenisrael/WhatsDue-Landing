952a2f4d7c4c98119b894719bcefd82e779c13ce
Dan Green <dan@tlvwebdevelopment.com>
2015-06-02 10:49:23 +0300

    Added testimonials

--- live-content/support.php
+++ live-content/support.php
@@ -2,11 +2,3 @@
 <h3>
      If you have any questions or issues, shoot Aaron an email: <a href="mailto:aaron@whatsdueapp.com">aaron@whatsdueapp.com</a>
 </h3>
-<br/>
-<h3>
-    If you don't see your syllabus, also <a href="mailto:aaron@whatsdueapp.com">email Aaron</a>
-</h3>
-<br>
-<h3>
-    <a onclick="showWelcome()">Click to show the Welcome Screen</a>
-</h3>
--- live-content/welcome-slider.php
+++ live-content/welcome-slider.php
@@ -8,56 +8,47 @@
         $options .= "<option value='$school'>$school</option>\n";
     }
 ?>
-<div class="swiper-container">
-    <div class="swiper-wrapper">
-        <div class="swiper-slide swiper-no-swiping">
-            <div class="content-slide start">
-                <h2>Find Your School</h2>
-                <form>
-                    <select id="schoolName">
-                        <?php echo $options;?>
-                    </select>
-                </form>
-                <p class="button my-school hidden">
-                    I go to <br/><i><strong id="school-name"></strong></i>
-                </p>
-            </div>
-        </div>
+<!--<div class="swiper-container">-->
+<!--    <div class="swiper-wrapper">-->
+<!---->
+<!--        <div class="swiper-slide">-->
+<!--            <div class="content-slide next">-->
+<!--                <h2>ADD ALL YOUR COURSES</h2>-->
+<!--                <p>-->
+<!--                    Add course by Course Code-->
+<!--                </p>-->
+<!--                <p>-->
+<!--                    Tell us if your course isn't there-->
+<!--                </p>-->
+<!--                <img src="http://whatsdueapp.com/live-content/assets/onboarding/add-course.png">-->
+<!--                <p class="button">-->
+<!--                    NEXT-->
+<!--                </p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="swiper-slide">-->
+<!--            <div class="content-slide finish">-->
+<!--                <h2>Check What's Due</h2>-->
+<!--                <p>-->
+<!--                    Swipe stuff away when you finish-->
+<!--                </p>-->
+<!--                <p>-->
+<!--                    Double Tap for more options-->
+<!--                </p>-->
+<!--                <img src="http://whatsdueapp.com/live-content/assets/onboarding/swipe-remove.png">-->
+<!--                <p class="button">-->
+<!--                    ADD A CLASS-->
+<!--                </p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!---->
+<!---->
+<!---->
+<!--    </div>-->
+<!--</div>-->
+<!--<div class="pagination"></div>-->
 
-        <div class="swiper-slide">
-            <div class="content-slide next">
-                <h2>ADD ALL YOUR COURSES</h2>
-                <p>
-                    Search by instructor or course name
-                </p>
-                <p>
-                    Tell us if your course isn't there
-                </p>
-                <img src="http://whatsdueapp.com/live-content/assets/onboarding/add-course.png">
-                <p class="button">
-                    NEXT
-                </p>
-            </div>
-        </div>
-
-        <div class="swiper-slide">
-            <div class="content-slide finish">
-                <h2>Check What's Due</h2>
-                <p>
-                    Swipe stuff away when you finish
-                </p>
-                <p>
-                    Double Tap for more options
-                </p>
-                <img src="http://whatsdueapp.com/live-content/assets/onboarding/swipe-remove.png">
-                <p class="button">
-                    ADD A CLASS
-                </p>
-            </div>
-        </div>
-
-
-
-    </div>
-</div>
-<div class="pagination"></div>
+<script>
+    $('#welcome').hide();
+</script>
